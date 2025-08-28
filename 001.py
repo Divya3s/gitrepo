162bfb98885e4f8a6cb8094e03dde8d227d5bf7a
Divya3s <shambika.divya@gmail.com>
2025-08-28 22:22:10 +0530

    masterfile

---
+++ 001.py
@@ -0,0 +1,16 @@
+# 1. Reverse a String 👉 Slices the string from end to start
+
+def reverse_string(s):
+    return s[::-1]
+
+# Example
+print(reverse_string("hello"))  # Output: "olleh"
+
+
+def revstr(s):
+    st = s[::-1]
+    return st
+
+print(revstr("world"))
+
+
